a756bc13402712798a4312677b907a992a516ac7
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-09 16:25:45 +0700

    Create LibraryApplication.java

---
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package com.projectcapstone.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
