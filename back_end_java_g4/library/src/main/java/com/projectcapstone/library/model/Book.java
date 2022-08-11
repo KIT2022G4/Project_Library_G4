d40327d459af2a923e2dbb515538ca0f428d1140
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-11 16:46:18 +0700

    Create Book.java

---
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/model/Book.java
@@ -0,0 +1,46 @@
+package com.projectcapstone.library.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.List;
+
+
+@Data
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idbook;
+
+    @Column(name = "bookname", nullable = false)
+    private String bookname;
+
+    private String author;
+
+    private String publisher;
+
+    private String introduction;
+
+    @Column(columnDefinition = "DATE")
+    private Date dor;
+
+    @Column(name = "bookimage", columnDefinition = "varchar(255) default 'https://khothietke.net/wp-content/uploads/2021/05/PNGkhothietke.net-02705.png'")
+    private String bookimage;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "idcategory", nullable = false)
+    private Category category;
+
+    @OneToMany(mappedBy = "book",cascade = CascadeType.ALL)
+    private List<BranchBook> branchBookList;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<Rating> ratingList;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL)
+    private List<DetailBook> detailBookList;
+
+}
