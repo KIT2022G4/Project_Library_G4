9354e5016ea3655364decd3d34728b0475b703a3
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-11 16:46:42 +0700

    Create OrderBook.java

---
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/model/OrderBook.java
@@ -0,0 +1,33 @@
+package com.projectcapstone.library.model;
+
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "orderbook")
+public class OrderBook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idorderbook;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "iddetailbook", nullable = false)
+    private DetailBook detailbook;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "iduser", nullable = false)
+    private User user;
+
+    @Column(columnDefinition = "DATE DEFAULT (CURRENT_DATE)")
+    private Date starttime;
+
+    @Column(columnDefinition = "DATE")
+    private Date datetime;
+
+    @Column(columnDefinition = "DATE")
+    private Date endtime;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean status;
+}
