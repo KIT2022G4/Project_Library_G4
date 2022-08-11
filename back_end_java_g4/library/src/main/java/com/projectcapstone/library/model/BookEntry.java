a3ca13690c99befeb212568d257b3a722be2abef
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-11 16:46:25 +0700

    Create BookEntry.java

---
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/model/BookEntry.java
@@ -0,0 +1,29 @@
+package com.projectcapstone.library.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@Entity
+@Table(name = "bookentry")
+public class BookEntry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer identry;
+
+    private String bookname;
+
+    private String author;
+
+//    date of requirements
+    @Column(columnDefinition = "DATE DEFAULT (CURRENT_DATE)")
+    private Date dor;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "iduser", nullable = false)
+    private User user;
+
+
+}
