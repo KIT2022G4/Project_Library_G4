4ad74a9049b0abdf72e529113c92a8a9f0dda97a
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-11 16:46:33 +0700

    Create Category.java

---
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/model/Category.java
@@ -0,0 +1,24 @@
+package com.projectcapstone.library.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idcategory;
+
+    @Column(nullable = false, unique = true)
+    private String categoryname;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Book> books;
+
+}
+
