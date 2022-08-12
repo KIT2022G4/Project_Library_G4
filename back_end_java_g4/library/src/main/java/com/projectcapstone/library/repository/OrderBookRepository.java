25d3e5e09f674d41436c54d8f86a7682f5bdf079
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-12 10:04:34 +0700

    Create OrderBookRepository.java

---
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/OrderBookRepository.java
@@ -0,0 +1,7 @@
+package com.projectcapstone.library.repository;
+
+import com.projectcapstone.library.model.OrderBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderBookRepository extends JpaRepository<OrderBook, Integer> {
+}
