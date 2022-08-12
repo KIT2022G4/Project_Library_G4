268a18126fc9cb56b7eb9a02166ac1e646302fc8
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-12 10:04:30 +0700

    Create CategoryRepository.java

---
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.projectcapstone.library.repository;
+
+import com.projectcapstone.library.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
