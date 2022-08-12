87ca813c79459db5c6e9377af020ec11afc7f876
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-12 13:17:31 +0700

    Delete DetailRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/DetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projectcapstone.library.repository;
-
-import com.projectcapstone.library.model.DetailBook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetailRepository extends JpaRepository<DetailBook, String> {
-}
