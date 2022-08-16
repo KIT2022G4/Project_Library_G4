e43d6239f3f57287d68eca451c8d49101c8f9edc
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:30 +0700

    Update UserStatusRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/UserStatusRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/UserStatusRepository.java
@@ -2,6 +2,8 @@ package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserStatusRepository extends JpaRepository<UserStatus, Long> {
 }
