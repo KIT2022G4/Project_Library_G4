452b5d4b463c62ccde0702ba6966d0881bc4c1b3
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:22 +0700

    Update DetailBookRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/DetailBookRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/DetailBookRepository.java
@@ -2,6 +2,8 @@ package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.DetailBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DetailBookRepository extends JpaRepository<DetailBook, Long> {
 }
