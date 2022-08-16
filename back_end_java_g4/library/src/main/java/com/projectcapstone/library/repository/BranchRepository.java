ca2afcd382e90aa5f4f4e30b019e5d953822764f
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 09:12:15 +0700

    Update BranchRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/BranchRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/BranchRepository.java
@@ -3,5 +3,5 @@ package com.projectcapstone.library.repository;
 import com.projectcapstone.library.model.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BranchRepository extends JpaRepository<Branch, Integer> {
+public interface BranchRepository extends JpaRepository<Branch, Long> {
 }
