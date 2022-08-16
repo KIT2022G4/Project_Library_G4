3836033f3fa6dec232ffc17679fea3714887e8ff
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:54:08 +0700

    Update BookRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/BookRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/BookRepository.java
@@ -1,9 +1,15 @@
 package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.Book;
+import com.projectcapstone.library.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("select b from Book b where b.category.idcategory = ?1 ")
+    List<Book> bookByCategory(Long idCategory);
 }
