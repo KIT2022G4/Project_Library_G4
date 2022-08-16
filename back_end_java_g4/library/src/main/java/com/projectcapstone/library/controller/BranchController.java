937449b2ab2f5ca4d33a92ff7311a00c76107060
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:01 +0700

    Update BranchController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BranchController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BranchController.java
@@ -1,4 +1,28 @@
 package com.projectcapstone.library.controller;
 
+import com.projectcapstone.library.model.Branch;
+import com.projectcapstone.library.model.Rating;
+import com.projectcapstone.library.repository.BranchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class BranchController {
+    @Autowired
+    private BranchRepository branchRepository;
+
+    @GetMapping("/branch")
+    public List<Branch> getAllBranches() {
+        return branchRepository.findAll();
+    }
+
+    @PostMapping("/branch")
+    public Branch createBranch(@RequestBody Branch branch) {
+        return branchRepository.save(branch);
+    }
+
 }
