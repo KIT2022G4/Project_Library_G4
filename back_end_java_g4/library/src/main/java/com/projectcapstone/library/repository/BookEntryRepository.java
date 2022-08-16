5e126e9124de2c26d90b2eee97fde363eda88c0e
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:12 +0700

    Update BookEntryRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/BookEntryRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/BookEntryRepository.java
@@ -2,6 +2,8 @@ package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.BookEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookEntryRepository extends JpaRepository<BookEntry, Long> {
 }
