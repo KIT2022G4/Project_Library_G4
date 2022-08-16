e79fd66d7a88983a5355bc373b58190349913725
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:19 +0700

    Update CategoryRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/CategoryRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
