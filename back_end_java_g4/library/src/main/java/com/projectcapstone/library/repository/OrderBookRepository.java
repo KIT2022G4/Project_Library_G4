c7f22474244c81201bf1d3f451e991daa01fb50d
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:23 +0700

    Update OrderBookRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/OrderBookRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/OrderBookRepository.java
@@ -1,7 +1,14 @@
 package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.OrderBook;
+import com.projectcapstone.library.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface OrderBookRepository extends JpaRepository<OrderBook, Long> {
+    @Query("select odb from OrderBook odb where odb.user.codeuser = ?1 ")
+    List<OrderBook> orderBookByCodeUser(String codeUser);
 }
