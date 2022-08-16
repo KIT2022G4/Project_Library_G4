1a980e8dddce06803e5555d7fe01e57482cb84da
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:10 +0700

    Update UserStatusController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/UserStatusController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/UserStatusController.java
@@ -1,4 +1,28 @@
 package com.projectcapstone.library.controller;
 
+import com.projectcapstone.library.model.Rating;
+import com.projectcapstone.library.model.UserStatus;
+import com.projectcapstone.library.repository.RatingRepository;
+import com.projectcapstone.library.repository.UserStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class UserStatusController {
+    @Autowired
+    private UserStatusRepository userStatusRepository;
+
+    @GetMapping("/userStatus")
+    public List<UserStatus> getAllUserStatus() {
+        return userStatusRepository.findAll();
+    }
+
+    @PostMapping("/userStatus")
+    public UserStatus createUserStatus(@RequestBody UserStatus userStatus) {
+        return userStatusRepository.save(userStatus);
+    }
 }
