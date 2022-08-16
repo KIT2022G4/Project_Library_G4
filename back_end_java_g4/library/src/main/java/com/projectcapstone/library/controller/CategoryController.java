9c1375da72eddf94b769d2200b34f029472abaea
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:02 +0700

    Update CategoryController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/CategoryController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/CategoryController.java
@@ -1,4 +1,29 @@
 package com.projectcapstone.library.controller;
 
+import com.projectcapstone.library.model.Branch;
+import com.projectcapstone.library.model.Category;
+import com.projectcapstone.library.repository.BranchRepository;
+import com.projectcapstone.library.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/category")
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @PostMapping("/category")
+    public Category createCategory(@RequestBody Category category) {
+        return categoryRepository.save(category);
+    }
+
 }
