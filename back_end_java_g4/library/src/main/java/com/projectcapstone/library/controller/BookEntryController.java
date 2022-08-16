80fd3e52a4538936bb07b9d0c6d93848f73880b0
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:42:57 +0700

    Update BookEntryController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BookEntryController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BookEntryController.java
@@ -1,4 +1,29 @@
 package com.projectcapstone.library.controller;
 
+import com.projectcapstone.library.model.BookEntry;
+import com.projectcapstone.library.model.Branch;
+import com.projectcapstone.library.repository.BookEntryRepository;
+import com.projectcapstone.library.repository.BranchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class BookEntryController {
+    @Autowired
+    private BookEntryRepository bookEntryRepository;
+
+    @GetMapping("/bookEntry")
+    public List<BookEntry> getAllBookEntrys() {
+        return bookEntryRepository.findAll();
+    }
+
+    @PostMapping("/bookEntry")
+    public BookEntry createBookEntry(@RequestBody BookEntry bookEntry) {
+        return bookEntryRepository.save(bookEntry);
+    }
+
 }
