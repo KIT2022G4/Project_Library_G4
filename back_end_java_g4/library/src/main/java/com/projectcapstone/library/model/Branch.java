1a5f063e4380adfacfc9d1f63e8f355c6bb3df73
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 15:47:06 +0700

    Update Branch.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/model/Branch.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/model/Branch.java
@@ -1,15 +1,17 @@
 package com.projectcapstone.library.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
 
 
 @Data
 @Entity
 @Table(name = "branch")
-public class Branch {
+public class Branch implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idbranch;
@@ -18,6 +20,7 @@ public class Branch {
     private String branchname;
 
     @OneToMany(mappedBy = "branch",cascade = CascadeType.ALL)
+    @JsonIgnore
     private List<BranchBook> branchBookList;
 
     @Column(nullable = false, unique = true)
