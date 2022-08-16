a67df055c5a2c1dd28c0d542fabb11f6ad14d304
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:08 +0700

    Update RatingController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/RatingController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/RatingController.java
@@ -1,15 +1,12 @@
 package com.projectcapstone.library.controller;
 
 import com.projectcapstone.library.model.Rating;
-import com.projectcapstone.library.model.User;
 import com.projectcapstone.library.repository.RatingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 @CrossOrigin(origins="http://localhost:3000")
 @RestController
