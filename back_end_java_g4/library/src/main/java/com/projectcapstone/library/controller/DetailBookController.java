edbfc31e81c37463d7465a2fc31b41539f1b9cc7
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:04 +0700

    Update DetailBookController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/DetailBookController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/DetailBookController.java
@@ -1,4 +1,28 @@
 package com.projectcapstone.library.controller;
 
+import com.projectcapstone.library.model.Category;
+import com.projectcapstone.library.model.DetailBook;
+import com.projectcapstone.library.repository.CategoryRepository;
+import com.projectcapstone.library.repository.DetailBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class DetailBookController {
+    @Autowired
+    private DetailBookRepository detailBookRepository;
+
+    @GetMapping("/detailBook")
+    public List<DetailBook> getAllDetailBooks() {
+        return detailBookRepository.findAll();
+    }
+
+    @PostMapping("/detailBook")
+    public DetailBook createDetailBook(@RequestBody DetailBook detailBook) {
+        return detailBookRepository.save(detailBook);
+    }
 }
