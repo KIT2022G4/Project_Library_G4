16c416b0ea233770c66677551b484cd45f97d941
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:42:59 +0700

    Update BranchBookController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BranchBookController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BranchBookController.java
@@ -1,4 +1,32 @@
 package com.projectcapstone.library.controller;
 
+import com.projectcapstone.library.model.BranchBook;
+import com.projectcapstone.library.repository.BranchBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class BranchBookController {
+    @Autowired
+    private BranchBookRepository branchBookRepository;
+
+    @GetMapping("/branchBook")
+    public List<BranchBook> getAllBranchBooks() {
+        return branchBookRepository.findAll();
+    }
+
+    @PostMapping("/branchBook")
+    public BranchBook createBranchBook(@RequestBody BranchBook branchBook) {
+        return branchBookRepository.save(branchBook);
+    }
+
+    @GetMapping("/branchBookByCodeBook")
+    public List<BranchBook> getAllBranchBookByCodeBook(@RequestParam String codeBook) {
+        return branchBookRepository.branchBookByCodeBook(codeBook);
+    }
+
 }
