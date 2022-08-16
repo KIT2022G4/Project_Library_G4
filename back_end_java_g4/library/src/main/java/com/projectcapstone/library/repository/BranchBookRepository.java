9bd00644db175d4d5f9fda5e5876271c28259542
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 16:43:15 +0700

    Update BranchBookRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/BranchBookRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/BranchBookRepository.java
@@ -2,7 +2,14 @@ package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.BranchBook;
 import com.projectcapstone.library.model.IdBranchBook;
+import com.projectcapstone.library.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface BranchBookRepository extends JpaRepository<BranchBook, IdBranchBook> {
+    @Query("select brb from BranchBook brb where brb.book.codebook = ?1")
+    List<BranchBook> branchBookByCodeBook(String codeBook);
 }
