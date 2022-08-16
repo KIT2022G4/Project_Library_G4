c94d530e3c834f9af1dd3c76fe88dff434f9bba4
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-16 15:47:22 +0700

    Update UserRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/UserRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("select u from User u where u.username = ?1 and u.password = ?2")
+    User login(String username, String password);
 }
