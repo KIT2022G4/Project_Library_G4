b59482f0b9e98275ff986f1951b6525364b9636d
thutra1999 <trathutran9@gmail.com>
2022-08-17 17:35:48 +0700

    17/8

--- front_end_react_g4/src/App.js
+++ front_end_react_g4/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import ListProduct from './pages/ListProduct/ListProduct';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import Profile from './pages/Profile/Profile';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ListProduct />} />
+      {/* <Route path="login" element={<Login />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="signup" element={<SignUp />} /> */}
+    </Routes>
+  </BrowserRouter>
+    </>
   );
 }
 
--- front_end_react_g4/src/pages/ListProduct/ListProduct.js
+++ front_end_react_g4/src/pages/ListProduct/ListProduct.js
@@ -1,4 +1,4 @@
-import '.ListProduct.css';
+import './ListProduct.css';
 import React from "react";
 
 const ListProduct = () => {
@@ -7,8 +7,7 @@ const ListProduct = () => {
             <div class="container-fluid">
                 <div class="container">
                     {/* Breadcrumb  */}
-                    <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);"
-                        aria-label="breadcrumb">
+                    <nav style={{}} aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Thư viện sách</li>
@@ -18,7 +17,7 @@ const ListProduct = () => {
                     <div class="row">
                         <div class="col-sm-4">
 
-                            <div class="card" style="width: 26rem;">
+                            <div class="card" style={{ width: "26rem" }} >
                                 <div class="card-header">
                                     Thống kê
                                 </div>
@@ -86,7 +85,7 @@ const ListProduct = () => {
                             <div class="alert alert-primary" role="alert">
                                 Danh mục sách
                             </div>
-                            <div class="card mb-3" style="max-width: 1000px;">
+                            <div class="card mb-3" style={{ maxWidth: "1000px" }} >
                                 <div class="row g-0">
                                     <div class="col-md-4">
                                         <img src="https://thuvienso.hcmulaw.edu.vn/wpViewImage.ashx?Id=2251"
@@ -123,7 +122,7 @@ const ListProduct = () => {
                             </div>
 
                             <div>
-                                <div class="card mb-3" style="max-width: 1000px;">
+                                <div class="card mb-3" style={{ maxWidth: "1000px" }}>
                                     <div class="row g-0">
                                         <div class="col-md-4">
                                             <img src="https://thuvienso.hcmulaw.edu.vn/wpViewImage.ashx?Id=2251"
@@ -161,7 +160,7 @@ const ListProduct = () => {
                             </div>
 
                             <div>
-                                <div class="card mb-3" style="max-width: 1000px;">
+                                <div class="card mb-3" style={{ maxWidth: "1000px" }}>
                                     <div class="row g-0">
                                         <div class="col-md-4">
                                             <img src="https://thuvienso.hcmulaw.edu.vn/wpViewImage.ashx?Id=2251"
@@ -199,7 +198,7 @@ const ListProduct = () => {
                             </div>
 
                             <div>
-                                <div class="card mb-3" style="max-width: 1000px;">
+                                <div class="card mb-3" style={{ maxWidth: "1000px" }}>
                                     <div class="row g-0">
                                         <div class="col-md-4">
                                             <img src="https://thuvienso.hcmulaw.edu.vn/wpViewImage.ashx?Id=2251"
@@ -238,7 +237,6 @@ const ListProduct = () => {
                         </div>
                     </div>
                 </div>
-
             </div>
         </>
     )
