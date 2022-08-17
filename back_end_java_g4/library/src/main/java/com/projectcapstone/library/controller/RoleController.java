81c4f40c3f0d8d2bec08bc1d3ddd671fc7bce333
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-17 17:38:07 +0700

    Update RoleController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/RoleController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/RoleController.java
@@ -1,4 +1,11 @@
 package com.projectcapstone.library.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins="http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class RoleController {
 }
