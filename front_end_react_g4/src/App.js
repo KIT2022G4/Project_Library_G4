40eb5c2333f9b5e8633c5e1c7013030967266715
PVQuyet <93429125+PVQuyet@users.noreply.github.com>
2022-08-17 17:32:13 +0700

    Update App.js

--- front_end_react_g4/src/App.js
+++ front_end_react_g4/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home_guess from './Page/Home/home_guess';
+import Detail from './Page/Detail/Detail';
+import Customers from "./Component/Customer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home_guess />} />
+          <Route path="detail" element={<Detail />} />
+          <Route path="edit" element={<Customers />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
