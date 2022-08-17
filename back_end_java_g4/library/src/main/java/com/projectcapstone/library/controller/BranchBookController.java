3ff711a03dab38373fa8e6b6e899ef8c0de9f82c
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-17 16:26:55 +0700

    Update BranchBookController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BranchBookController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BranchBookController.java
@@ -1,7 +1,9 @@
 package com.projectcapstone.library.controller;
 
 import com.projectcapstone.library.model.BranchBook;
+import com.projectcapstone.library.model.DetailBook;
 import com.projectcapstone.library.repository.BranchBookRepository;
+import com.projectcapstone.library.repository.DetailBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,8 @@ import java.util.List;
 public class BranchBookController {
     @Autowired
     private BranchBookRepository branchBookRepository;
+    @Autowired
+    private DetailBookRepository detailBookRepository;
 
     @GetMapping("/branchBook")
     public List<BranchBook> getAllBranchBooks() {
@@ -21,7 +25,15 @@ public class BranchBookController {
 
     @PostMapping("/branchBook")
     public BranchBook createBranchBook(@RequestBody BranchBook branchBook) {
-        return branchBookRepository.save(branchBook);
+        BranchBook bb = branchBookRepository.save(branchBook);
+        for(int i = 1; i<= branchBook.getCount();i++){
+            DetailBook db = new DetailBook();
+            db.setBook(branchBook.getBook());
+            db.setBranch(branchBook.getBranch());
+            db.setCodedetailbook(branchBook.getBook().getCodebook() + i);
+            detailBookRepository.save(db);
+        }
+        return bb;
     }
 
     @GetMapping("/branchBookByIdBook/{id}")
