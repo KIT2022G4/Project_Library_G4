0765c73a582f0b20717e30776f580dfee922f7bb
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-17 16:28:18 +0700

    Update OrderBookRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/OrderBookRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/OrderBookRepository.java
@@ -2,6 +2,7 @@ package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.OrderBook;
 import com.projectcapstone.library.model.User;
+import com.projectcapstone.library.model.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,8 @@ import java.util.List;
 public interface OrderBookRepository extends JpaRepository<OrderBook, Long> {
     @Query("select odb from OrderBook odb where odb.user.iduser = ?1 ")
     List<OrderBook> orderBookByCodeUser(Long codeUser);
+
+    @Query("select odb from OrderBook odb where odb.datetime < current_date and odb.status = 2")
+    List<OrderBook> listOrderBookLate();
+
 }
