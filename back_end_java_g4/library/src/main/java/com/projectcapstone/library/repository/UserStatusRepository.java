995dd439cf83874d706563837de98e921a463b2b
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-17 16:28:21 +0700

    Update UserStatusRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/UserStatusRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/UserStatusRepository.java
@@ -2,8 +2,14 @@ package com.projectcapstone.library.repository;
 
 import com.projectcapstone.library.model.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserStatusRepository extends JpaRepository<UserStatus, Long> {
+    @Query("select ub from UserStatus ub where ub.status = false and ub.timeupdate < current_date")
+    List<UserStatus> listUserStatus();
+
 }
