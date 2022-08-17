63c433e9cbf44e6f44a1dc10df54e3ef3d933f7d
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-17 16:27:09 +0700

    Update LibraryApplication.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/LibraryApplication.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/LibraryApplication.java
@@ -2,13 +2,11 @@ package com.projectcapstone.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-import java.util.List;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-
+@EnableScheduling
 public class LibraryApplication {
 
 	public static void main(String[] args) {
