2ffb45c0d02f7fd956198f9a0e2584e55ee6a526
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-17 16:27:03 +0700

    Update CategoryController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/CategoryController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/CategoryController.java
@@ -1,13 +1,17 @@
 package com.projectcapstone.library.controller;
 
+import com.projectcapstone.library.model.Book;
 import com.projectcapstone.library.model.Branch;
 import com.projectcapstone.library.model.Category;
 import com.projectcapstone.library.repository.BranchRepository;
 import com.projectcapstone.library.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins="http://localhost:3000")
 @RestController
@@ -26,4 +30,13 @@ public class CategoryController {
         return categoryRepository.save(category);
     }
 
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<Map< String, Boolean >> deleteCategory(@PathVariable Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow();
+
+        categoryRepository.delete(category);
+        Map <String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 }
