522e90c626d76d3e63215a6f6bcfab9896516b6d
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-18 10:45:42 +0700

    Update App.js

--- front_end_react_g4/src/App.js
+++ front_end_react_g4/src/App.js
@@ -1,11 +1,17 @@
 
-import QRCode from "react-qr-code";
-import Detail from "./Page/Detail/Detail";
 import Home_guess from "./Page/Home/Home_guess";
 
 function App() {
   return (
-    <Home_guess/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home_guess />} />
+          <Route path="detail" element={<Detail />} />
+          <Route path="edit" element={<Customers />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
