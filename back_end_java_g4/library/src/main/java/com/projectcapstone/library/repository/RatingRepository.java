9d9343fa39492dcf64bb09097f2b9fb17620a7c6
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-18 10:37:30 +0700

    Update RatingRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/RatingRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/RatingRepository.java
@@ -1,9 +1,15 @@
 package com.projectcapstone.library.repository;
 
+import com.projectcapstone.library.model.Book;
 import com.projectcapstone.library.model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+    @Query("select r from Rating r where r.book.idbook = ?1 ")
+    List<Rating> ratingListByIdBook(Long idBook);
 }
