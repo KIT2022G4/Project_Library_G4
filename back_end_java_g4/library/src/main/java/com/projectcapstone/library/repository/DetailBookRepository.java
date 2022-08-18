b572d760549b0a31eebd4116339f451fadf2cd46
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-18 14:52:47 +0700

    Update DetailBookRepository.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/DetailBookRepository.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/repository/DetailBookRepository.java
@@ -1,9 +1,18 @@
 package com.projectcapstone.library.repository;
 
+import com.projectcapstone.library.model.Book;
 import com.projectcapstone.library.model.DetailBook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DetailBookRepository extends JpaRepository<DetailBook, Long> {
+    @Query(value = "select db from DetailBook db where db.status = true and db.book.idbook = ?1 and db.branch.idbranch = ?2 group by db.book.codebook")
+    DetailBook detailBookTop(Long idBook, Long idBranch);
+
+    @Query(value = "select db from DetailBook db where db.status = true and db.book.idbook = ?1 and db.branch.idbranch = ?2")
+    List<DetailBook> detailBook(Long idBook, Long idBranch);
 }
