bdb2220b4c58829f52bfed7e3aadcfc5b57d15c9
thutra1999 <trathutran9@gmail.com>
2022-08-18 14:11:39 +0700

    tra

--- front_end_react_g4/src/App.js
+++ front_end_react_g4/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import ListProduct from './pages/ListProduct/ListProduct';
-import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
-import Profile from './pages/Profile/Profile';
+import ListProduct from './Page/ListProduct/ListProduct';
+import Detail from './Page/Detail/Detail'
+import Login from './Page/Login/Login';
+import SignUp from './Page/SignUp/SignUp';
+import Profile from './Page/Profile/Profile';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home_guess from "./Page/Home/Home_guess";
+import Home_guess from "./Page/Home/home_guess";
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
         <Route path="/">
           <Route index element={<Home_guess />} />
           <Route path="detail" element={<Detail />} />
-          <Route path="edit" element={<Customers />} />
-          <Route path="/" element={<ListProduct />} />
+          {/* <Route path="edit" element={<Customers />} /> */}
+          <Route path="listproduct" element={<ListProduct />} />
           <Route path="login" element={<Login />} />
           <Route path="profile" element={<Profile />} />
           <Route path="signup" element={<SignUp />} />
