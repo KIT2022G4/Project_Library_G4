d55c83c5ab05c8b39d3fd4f2255749bcd9978c30
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-18 14:52:49 +0700

    Update ScheduledTasks.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/schedule/ScheduledTasks.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/schedule/ScheduledTasks.java
@@ -2,8 +2,10 @@ package com.projectcapstone.library.schedule;
 
 
 import com.projectcapstone.library.model.OrderBook;
+import com.projectcapstone.library.model.User;
 import com.projectcapstone.library.model.UserStatus;
 import com.projectcapstone.library.repository.OrderBookRepository;
+import com.projectcapstone.library.repository.UserRepository;
 import com.projectcapstone.library.repository.UserStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -20,37 +22,39 @@ public class ScheduledTasks {
     @Autowired
     private OrderBookRepository orderBookRepository;
 
+    @Autowired
+    private UserRepository userRepository;
 
-//    @Scheduled(fixedDelay = 1000)
-//    public void updateUserStatus(){
-//        List<OrderBook> orderBookList = orderBookRepository.listOrderBookLate();
+    @Scheduled(fixedDelay = 10000)
+    public void updateUserStatus(){
+//        List<OrderBook> orderBookList = orderBookRepository.findAll();
 //        for (OrderBook o: orderBookList){
 //            System.out.println(o.toString());
 //        }
-////        Date now = new Date(new java.util.Date().getTime());
-////        for(OrderBook ob:orderBookList){
-////            UserStatus us = userStatusRepository.getById(ob.getUser().getIduser());
-////            System.out.println(ob.toString());
-////            if(us.getTimeupdate().before(now)){
-////                us.setCrime(us.getCrime() + 1);
-////                us.setTimeupdate(now);
-////                us.setStatus(false);
-////                userStatusRepository.save(us);
-////            }
-////        }
-////
-////        List<UserStatus> userStatusList = userStatusRepository.listUserStatus();
-////        for (UserStatus us:userStatusList){
-////            Integer check = us.getCrime() - 1;
-////            if(check > 0){
-////                us.setCrime(check);
-////                us.setTimeupdate(now);
-////            }else {
-////                us.setCrime(0);
-////                us.setStatus(true);
-////                us.setTimeupdate(now);
-////            }
-////            userStatusRepository.save(us);
-////        }
-//    }
+        Date now = new Date(new java.util.Date().getTime());
+//        for(OrderBook ob:orderBookList){
+//            UserStatus us = userStatusRepository.getById(ob.getUser().getIduser());
+//            System.out.println(ob.toString());
+//            if(us.getTimeupdate().before(now)){
+//                us.setCrime(us.getCrime() + 1);
+//                us.setTimeupdate(now);
+//                us.setStatus(false);
+//                userStatusRepository.save(us);
+//            }
+//        }
+//
+        List<UserStatus> userStatusList = userStatusRepository.listUserStatus();
+        for (UserStatus us:userStatusList){
+            Integer check = us.getCrime() - 1;
+            if(check > 0){
+                us.setCrime(check);
+                us.setTimeupdate(now);
+            }else {
+                us.setCrime(0);
+                us.setStatus(true);
+                us.setTimeupdate(now);
+            }
+            userStatusRepository.save(us);
+        }
+    }
 }
