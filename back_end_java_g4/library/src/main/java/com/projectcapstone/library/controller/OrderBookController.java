03cfdf52d3acdb1568d523648a8ef359a2d0f444
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-24 09:47:04 +0700

    Update OrderBookController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/OrderBookController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/OrderBookController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.sql.Date;
 import java.util.List;
 
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v1")
 public class OrderBookController {
