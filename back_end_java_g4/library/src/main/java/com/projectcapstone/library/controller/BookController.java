f5dc17b3492172a30256099e6fa71927911623bf
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-24 09:46:51 +0700

    Update BookController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BookController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/BookController.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v1")
 public class BookController {
