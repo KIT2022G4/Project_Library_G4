7049273369e85847228c7ed5c417412593faa5e3
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-24 16:18:33 +0700

    Update Header.js

--- front_end_react_g4/src/Components/Header/Header.js
+++ front_end_react_g4/src/Components/Header/Header.js
@@ -1,7 +1,10 @@
 import './Header.css';
 import React from 'react';
 import { Link } from "react-router-dom";
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 const Header = () => {
     return (
         <>
