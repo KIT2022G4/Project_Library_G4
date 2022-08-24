cc1d1dd7008bd9d2ab8e33e7cdaed3366dc7370c
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-24 09:47:12 +0700

    Update UserController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/UserController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
