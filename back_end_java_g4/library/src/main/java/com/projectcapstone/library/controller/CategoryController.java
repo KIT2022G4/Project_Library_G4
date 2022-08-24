e8d3b33ffe5d992141db31f4bc30ab83897e8599
lagiahuy <103893858+lagiahuy@users.noreply.github.com>
2022-08-24 09:47:00 +0700

    Update CategoryController.java

--- back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/CategoryController.java
+++ back_end_java_g4/library/src/main/java/com/projectcapstone/library/controller/CategoryController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryController {
